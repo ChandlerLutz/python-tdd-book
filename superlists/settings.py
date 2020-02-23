66d1bb60b37d135cd6fe89f499b2ee53d0a054a0
Chandler Lutz <chandler.lutz@gmail.com>
2020-02-23 10:55:28 -0800

    remove hack for ansible

--- superlists/settings.py
+++ superlists/settings.py
@@ -29,11 +29,6 @@ else:
     SECRET_KEY = 'insecure-key-for-dev'
     ALLOWED_HOSTS = []
 
-
-DEBUG = True
-SECRET_KEY = 'insecure-key-for-dev'    
-ALLOWED_HOSTS = ['*']
-
 # Application definition
 
 INSTALLED_APPS = [
