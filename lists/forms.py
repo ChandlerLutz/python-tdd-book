87fe4097b064b4d71dda5c3ab9b27e8c2f1331ce
Chandler Lutz <chandler.lutz@gmail.com>
2020-03-11 09:37:17 -0700

    Implement duplicate validation at model layer

--- lists/forms.py
+++ lists/forms.py
@@ -6,6 +6,10 @@ EMPTY_ITEM_ERROR = "You can't have an empty list item"
 
 class ItemForm(forms.models.ModelForm):
 
+    def save(self, for_list):
+        self.instance.list = for_list
+        return super().save()
+
     class Meta:
         model = Item
         fields = ('text', )
@@ -18,4 +22,4 @@ class ItemForm(forms.models.ModelForm):
         error_messages = {
             'text': {'required': EMPTY_ITEM_ERROR}
         }
-    
+
