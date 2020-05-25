4767b4d91157a6e8b7069ee09a21e3ce92cae0a2
Chandler Lutz <chandler.lutz@gmail.com>
2020-05-25 14:43:06 -0700

    fixed typo in test description comments

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -45,7 +45,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('2: Make tea')
 
     def test_cannot_add_duplicate_items(self):
-        # Edith goes to teh home page and starts a new list
+        # Edith goes to the home page and starts a new list
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys('Buy wellies')
         self.get_item_input_box().send_keys(Keys.ENTER)
